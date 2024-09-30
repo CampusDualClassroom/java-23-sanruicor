04e7f3d626299935b92ef3302cd2779799ab78b9
AdelaSRuizDeCortázar <sanruiade@gmail.com>
2024-09-30 13:56:21 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise23.java
+++ src/main/java/com/campusdual/classroom/Exercise23.java
@@ -6,15 +6,28 @@ import java.util.Map;
 public class Exercise23 {
 
     public static Map<String, Person> createHashMap() {
-
+        Map<String, Person> mapa = new HashMap<>();
+        mapa.put("person", new Person("John", "Smith"));
+        mapa.put("teacher", new Teacher("María", "Montessori", "Educación"));
+        mapa.put("police", new PoliceOfficer("Jake", "Peralta", "B-99"));
+        mapa.put("doctor", new Doctor("Gregory", "House", "Nefrología e infectología"));
+        return mapa;
     }
 
     public static Person addMapValue(Map<String, Person> map, String key, Person value) {
-
+        map.put(key, value);
+        return map.get(key);
     }
 
     public static void printMapValues(Map<String, Person> map) {
-
+        for (Person person : map.values()) {
+            person.getDetails();
+        }
     }
-    
+
+    public static void main(String[] args) {
+        Map<String, Person> mapa = createHashMap();
+        addMapValue(mapa, "police", new PoliceOfficer("Charles", "Boyle", "B-99"));  // no se añade porque ya existe la key "police"
+        printMapValues(mapa);
+    }
 }
